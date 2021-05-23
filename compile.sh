ee6c4c0e1e654da50539c1f47d8d0d2027daf896
philipp <50250716+Kuuhhl@users.noreply.github.com>
2021-05-23 12:48:13 +0200

    removed unnecessary message

--- compile.sh
+++ compile.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 make O=out ARCH=arm64 nethunter_defconfig
 
-
 # change paths here:
 clangPath="$HOME/linux-x86-android-10.0.0_r3-clang-r353983c/bin"
 gccArm64Path="$HOME/aarch64-linux-android-4.9/bin"
 gccArmPath="$HOME/arm-linux-androideabi-4.9/bin"
-echo $clangPath
 
 PATH="${clangPath}:${gccArm64Path}:${gccArmPath}:${PATH}" \
 make -j$(nproc --all) O=out \
